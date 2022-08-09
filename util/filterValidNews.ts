9b23bc5f23bf5bc25584e370486250e189ca2e2e
Muskaan <muskaan19010@gmail.com>
2022-08-09 13:07:19 +0530

    check if author is not null when filtering news

--- util/filterValidNews.ts
+++ util/filterValidNews.ts
@@ -2,6 +2,12 @@ import { TNews, TResponseData } from "./types";
 
 export const filterValidNews = (arr: TResponseData[]): TNews[] => {
   return arr.filter(
-    (news) => news.content && news.url && news.title && news.urlToImage && news
+    (news) =>
+      news.content &&
+      news.url &&
+      news.title &&
+      news.urlToImage &&
+      news.author &&
+      news
   );
 };
